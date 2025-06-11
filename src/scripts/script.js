a8616c134628a2d3b801516637b4b78779c6fd67
lyosinho <alexbarrosjj@hotmail.com>
2025-06-10 22:09:40 -0300

    Relogio finalizado

---
+++ src/scripts/script.js
@@ -0,0 +1,19 @@
+const hoursHand = document.getElementById('hours-hand');
+const minutesHand = document.getElementById('minutes-hand');
+const secondsHand = document.getElementById('seconds-hand');
+
+function clockTick() {
+    const date = new Date();
+    const seconds = date.getSeconds() / 60;
+    const minutes = (seconds + date.getMinutes()) / 60;
+    const hours = (minutes + date.getHours()) / 12;
+
+    rotateClockHands(hoursHand, hours); 
+    rotateClockHands(minutesHand, minutes);
+    rotateClockHands(secondsHand, seconds);
+}
+function rotateClockHands(element, rotation) {
+    element.style.transform = `translate(-50%, -100%) rotate(${rotation * 360}deg)`;
+
+}
+setInterval(clockTick, 1000);
